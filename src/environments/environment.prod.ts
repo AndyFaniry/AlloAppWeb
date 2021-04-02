dc0103bed46f888ffe36b0c19a37cd32c52b9bcd
Andy Faniry <ramarosonandyfaniry@gmail.com>
2021-04-02 23:17:52 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true
 };
+export let options = {
+  headers: {
+    'Content-Type' : 'application/json',
+    'Access-Control-Allow-Origin' : '*'
+  }
+};
+export const base_url="https://alloappweb.herokuapp.com/";
